601304107f24e7f0a8649a0bc89206c7c9f17271
root <liuch1207@sina.com>
2026-02-09 16:30:33 +0800

    Add optional sync for Notion and NeoDB.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-import { consola } from 'consola';
-import { fetchRSSFeeds, handleRSSFeeds } from './handle-rss';
-import handleNotion from './handle-notion';
-import handleNeodb from './handle-neodb';
-import { ItemStatus } from './types';
+import { consola } from "consola";
+import { fetchRSSFeeds, handleRSSFeeds } from "./handle-rss";
+import handleNotion from "./handle-notion";
+import handleNeodb from "./handle-neodb";
+import { ItemStatus } from "./types";
 
 async function main(): Promise<void> {
-  const feeds = await fetchRSSFeeds();
-  if (feeds.length === 0) {
-    consola.info('No new items.');
-    return;
-  }
+    const feeds = await fetchRSSFeeds();
+    if (feeds.length === 0) {
+        consola.info("No new items.");
+        return;
+    }
 
-  const normalizedFeeds = handleRSSFeeds(feeds);
-  const completeFeeds = normalizedFeeds.filter(f => f.status === ItemStatus.Complete);
+    const normalizedFeeds = handleRSSFeeds(feeds);
+    const completeFeeds = normalizedFeeds.filter(
+        (f) => f.status === ItemStatus.Complete,
+    );
 
-  if (completeFeeds.length) {
-    await handleNotion(completeFeeds);
-  }
+    if (completeFeeds.length && process.env.NOTION_TOKEN) {
+        await handleNotion(completeFeeds);
+    }
 
-  await handleNeodb(normalizedFeeds);
+    if (process.env.NEODB_API_TOKEN) {
+        await handleNeodb(normalizedFeeds);
+    }
 }
 
 main();
